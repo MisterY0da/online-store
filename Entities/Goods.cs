13348b6a144d054284eb7c7f846b1f5b8b9c25b8
MisterY0da <pretty_good.guy@mail.ru>
2020-05-29 00:58:50 +0300

    add Entity, Goods and OrdersHistory classes

---
+++ Entities/Goods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineStore.Entities
+{
+    class Goods : AuditableEntity
+    {
+        public string Name { get; set; }
+        public float Price { get; set; }
+
+        public Goods(string name, float price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        //Для EF Core
+        public Goods()
+        {
+
+        }
+    }
+}
---
+++ Entities/OrdersHistory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+
+namespace OnlineStore.Entities
+{
+    class OrdersHistory : AuditableEntity
+    {
+        public virtual ICollection<Order> OrdersList { get; set; }
+
+        public string getClientFullName()
+        {
+            return OrdersList.First().ClientName + " " + OrdersList.First().ClientLastName + " " + OrdersList.First().ClientPatronymic;
+        }
+
+        //Для EF Core
+        public OrdersHistory()
+        {
+
+        }
+
+    }
+}
