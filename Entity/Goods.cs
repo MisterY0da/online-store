e59ef1ff9253da39f46ca632987203a992018f66
Ekaterina <antonovaekate@gmail.com>
2020-05-29 20:35:11 +0300

    add .gitignore, update AuditableEntity, Entity, Goods, Order

---
+++ Entity/Goods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineStore.Entities
+{
+   public class Goods : AuditableEntity
+    {
+        public string Name { get; set; }
+        public float Price { get; set; }
+
+        public Goods(string name, float price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        //Для EF Core
+        public Goods()
+        {
+
+        }
+    }
+}
---
+++ Entity/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace OnlineStore.Entities
+{
+  public  class Order : AuditableEntity
+    {
+        public virtual ICollection<Goods> GoodsList { get; set; }
+        public virtual Client Client { get; set; }
+        public Order()
+        {
+
+        }
+
+    }
+}
