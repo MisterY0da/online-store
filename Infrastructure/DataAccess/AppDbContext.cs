a6e1fec210b59af40a23770a343853b81652fd89
Ekaterina <antonovaekate@gmail.com>
2020-05-31 01:52:18 +0300

    update Infrastructure project

---
+++ Infrastructure/DataAccess/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using OnlineStore.Entities;
+
+
+namespace Infrastructure.DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Client> Client { get; set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<Goods> Goods { get; set; }
+
+
+    }
+}
