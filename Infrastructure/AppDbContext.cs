d763144afdd4268b595807abceeebe49c8f51f68
Ekaterina <antonovaekate@gmail.com>
2020-05-31 01:12:59 +0300

    add Infrastructure project and AppDBContext.cs

---
+++ Infrastructure/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using OnlineStore.Entities;
+
+
+namespace Infrastructure.DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Client> Client { get; set; }
+
+        
+
+    }
+}
