ef743ec0b2023c026d2cf7e1cc0d8ed297f58cfd
Ekaterina <antonovaekate@gmail.com>
2020-05-31 13:42:27 +0300

    add Repository.cs, AuditableRepository.cs, IClientRepository.cs, ClientRepository.cs

---
+++ Infrastructure/DataAccess/ClientRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using OnlineStore.Entities;
+
+namespace Infrastructure.DataAccess
+{
+    class ClientRepository
+    {
+        public class ClientRepository : AuditableRepository<Client>, IClientRepository
+        {
+            private readonly AppDbContext _dbContext;
+            public ClientRepository(AppDbContext dbContext) : base(dbContext)
+            {
+                _dbContext = dbContext;
+            }
+
+            public override Client Get(int id)
+            {
+                return _dbContext.Clients.FirstOrDefault(c => c.Id == id);
+            }
+
+            public IReadOnlyList<Client> GetClientByFirstName(string firstName)
+            {
+                return _dbContext.Clients.Where(x => x.FirstName.ToLower().Contains(firstName.ToLower())).ToList();
+            }
+
+            public IReadOnlyList<Client> GetClientByLastName(string lastName)
+            {
+                return _dbContext.Clients.Where(x => x.LastName.ToLower().Contains(lastName.ToLower())).ToList();
+            }
+        }
+    }
+}
---
+++ Infrastructure/DataAccess/IClientRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OnlineStore.Entities;
+
+namespace Infrastructure.DataAccess
+{
+   public interface IClientRepository
+    {
+       
+        Client Get(int id);
+        void Add(Client client);
+        void Update(Client client);
+        IReadOnlyList<Client> GetClientByLastName(string lastName);
+        IReadOnlyList<Client> GetClientByFirstName(string firstName);
+    
+}
+}
