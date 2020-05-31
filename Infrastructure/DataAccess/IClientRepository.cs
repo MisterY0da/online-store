71f914f45624027a6ce84212b2240e92d3dc9dce
MisterY0da <pretty_good.guy@mail.ru>
2020-05-31 18:39:03 +0300

    add ProductRepository.cs, OrderRepository.cs

--- Infrastructure/DataAccess/IClientRepository.cs
+++ Infrastructure/DataAccess/IClientRepository.cs
@@ -5,14 +5,12 @@ using OnlineStore.Entities;
 
 namespace Infrastructure.DataAccess
 {
-   public interface IClientRepository
+    public interface IClientRepository
     {
-       
         Client Get(int id);
         void Add(Client client);
         void Update(Client client);
         IReadOnlyList<Client> GetClientByLastName(string lastName);
         IReadOnlyList<Client> GetClientByFirstName(string firstName);
-    
-}
+    }
 }
--- OnlineStore/Entities/Order.cs
+++ OnlineStore/Entities/Order.cs
@@ -7,12 +7,17 @@ namespace OnlineStore.Entities
 {
   public class Order : AuditableEntity
   {
-        //Order has client, show unique relationship to client
-        public int ClientId { get; set; }
+        //Order has client, show single relationship to client
         public virtual Client Client { get; set; }
 
         //Order has products, relationship 1 to many
         public virtual ICollection<Product> Products { get; set; }
+
+        public Order(Client client, DateTime createdAt)
+        {
+            Client = client;
+            CreatedAt = createdAt;
+        }
 
         //For EF Core
         public Order()
